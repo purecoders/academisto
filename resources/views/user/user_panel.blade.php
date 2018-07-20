31e2a803efde7d8917481323902b499371f95048
Pouya Aklion <pouyaakn4@gmail.com>
2018-07-20 21:01:22 +0430

    change header of card and add yield

--- resources/views/user/user_panel.blade.php
+++ resources/views/user/user_panel.blade.php
@@ -5,8 +5,8 @@
             <div class="card-header ">
                 <ul class="nav nav-tabs card-header-tabs">
                     <li class="active mr-auto"><a  href="{{route('user-ads')}}">آگهی های من</a></li>
-                    <li class="mr-auto"><a data-toggle="tab" href="#menu1">پروژه های درخواست داده شده</a></li>
-                    <li class="mr-auto"><a data-toggle="tab" href="#menu2">سفارش پروژه</a></li>
+                    <li class="mr-auto"><a href="{{route('user-orders')}}">سفارش پروژه</a></li>
+                    <li class="mr-auto"><a data-toggle="tab" href="#menu2">انجام پروژه</a></li>
                     <li class="mr-auto"><a data-toggle="tab" href="#menu3">ارسال رزومه</a></li>
                     <li class="mr-auto"><a data-toggle="tab" href="#menu3">امور مالی</a></li>
                     <li class="mr-auto"><a data-toggle="tab" href="#menu3">تیکت</a></li>
@@ -14,22 +14,17 @@
             </div>
             <div class="card-body">
                 <div class="tab-content">
-                    <div id="home" class="tab-pane fade in active show">
+                    <div id="ads" class="tab-pane fade in active show">
                         @yield('ads')
                     </div>
-                    <div id="menu1" class="tab-pane fade">
-                        <h3>منو</h3>
-                        <p>یک تغییر برای تست گیت میباشد.</p>
+                    <div id="order_project" class="tab-pane fade">
+                        @yield ('orders')
                     </div>
-                    <div id="menu2" class="tab-pane fade">
-                        <h3>Menu 2</h3>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                            totam rem aperiam.</p>
+                    <div id="do_project" class="tab-pane fade">
+                        @yield('do_project')
                     </div>
-                    <div id="menu3" class="tab-pane fade">
-                        <h3>Menu 3</h3>
-                        <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                            explicabo.</p>
+                    <div id="send_cv" class="tab-pane fade">
+                        @yield('cv')
                     </div>
         </div>
         </div>
