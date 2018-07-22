020c288c8becf2e2725930c804735b3b83aa744c
Pouya Aklion <pouyaakn4@gmail.com>
2018-07-22 11:38:47 +0430

    add finance tables

--- resources/views/user/finance.blade.php
+++ resources/views/user/finance.blade.php
@@ -22,4 +22,65 @@
         </div>
         <div class="col-md-6"></div>
     </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <h4>پرداخت های شما به <strong class="academisto-brand">آکادمیستو</strong> </h4>
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th  class="text-center" scope="col" >ردیف</th>
+                        <th   class="text-center" scope="col">تاریخ</th>
+                        <th class="text-center" scope="col">مبلغ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr class="text-center">
+                    <th class="text-center" scope="row ">1</th>
+                    <td>96/12/05</td>
+                    <td>185000</td>
+                </tr>
+                <tr class="text-center">
+                    <th class="text-center" scope="row ">2</th>
+                    <td>96/12/05</td>
+                    <td>185000</td>
+                </tr>
+                <tr class="text-center">
+                    <th class="text-center" scope="row ">3</th>
+                    <td>96/12/05</td>
+                    <td>185000</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-6">
+            <h4>پرداخت های <strong class="academisto-brand">آکادمیستو</strong>  به شما</h4>
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th  class="text-center" scope="col" >ردیف</th>
+                    <th   class="text-center" scope="col">تاریخ</th>
+                    <th class="text-center" scope="col">مبلغ</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="text-center">
+                    <th class="text-center" scope="row ">1</th>
+                    <td>96/12/05</td>
+                    <td>185000</td>
+                </tr>
+                <tr class="text-center">
+                    <th class="text-center" scope="row ">2</th>
+                    <td>96/12/05</td>
+                    <td>185000</td>
+                </tr>
+                <tr class="text-center">
+                    <th class="text-center" scope="row ">3</th>
+                    <td>96/12/05</td>
+                    <td>185000</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
