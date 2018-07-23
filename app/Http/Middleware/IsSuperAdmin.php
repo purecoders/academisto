c90a46e959e59861cc3e560048fec55ec08aed14
Pouya Aklion <pouyaakn4@gmail.com>
2018-07-23 19:43:46 +0430

    add super admin middleware

---
+++ app/Http/Middleware/IsSuperAdmin.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+class IsSuperAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        $roles = $user->roles;
+        $isSuperAdmin = false;
+        foreach ($roles as $role){
+            if($role->name == 'super_admin'){
+                $isSuperAdmin = true;
+                break;
+            }
+        }
+        if($isSuperAdmin){
+            return $next($request);
+        }else{
+            //redirect user to user-panel
+            return redirect('/user-panel');
+        }
+    }
+}
