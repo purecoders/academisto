062a26f4dc159349dd8e1b44f41952883fbaa8ef
Pouya Aklion <pouyaakn4@gmail.com>
2018-07-24 14:27:37 +0430

    add reports number to admin user ads

--- resources/views/admin/user_pages/ads.blade.php
+++ resources/views/admin/user_pages/ads.blade.php
@@ -7,111 +7,27 @@
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
                     <div class="card rtl">
-                        <img class="card-img-top  card-img-top-250" src="{{asset('assets/img/logo.png')}}" alt="Card image cap">
-                        <div class="card-body pb-1">
-                            <h5 class="card-title">کتاب شیمی</h5>
-                            <div id="summary">
-                                <p class="collapse" id="collapseSummary1">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
-                                </p>
-                                <a class="collapsed" data-toggle="collapse" href="#collapseSummary1" aria-expanded="false"
-                                   aria-controls="collapseSummary"></a>
+                        <div class="card-header bg-light p-1">
+                            <div class="d-flex justify-content-around">
+                                <a href="http://www.google.com" style="text-decoration: none;color:black">
+                                    <div >
+                                        <item >5</item>
+                                        <item class="text-danger" > گزارش </item>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-                        <ul class="list-group list-group-flush p-0 rtl">
-                            <li class="list-group-item">شهر: تهران</li>
-                            <li class="list-group-item">دانشگاه: دانشگاه شهید مدنی آذربایجان</li>
-                        </ul>
-                        <div class="card-footer p-1">
-                            <span class="card-info pr-4">قیمت:10000 تومان</span>
-                            <a href="#" class="btn btn-danger mr-auto">حذف آگهی</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card rtl">
-                        <img class="card-img-top  card-img-top-250" src="{{asset('assets/img/logo.png')}}" alt="Card image cap">
-                        <div class="card-body pb-1">
-                            <h5 class="card-title">کتاب شیمی</h5>
-                            <div id="summary">
-                                <p class="collapse" id="collapseSummary2">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
-                                </p>
-                                <a class="collapsed" data-toggle="collapse" href="#collapseSummary2" aria-expanded="false"
-                                   aria-controls="collapseSummary"></a>
-                            </div>
-                        </div>
-                        <ul class="list-group list-group-flush p-0 rtl">
-                            <li class="list-group-item">شهر: تهران</li>
-                            <li class="list-group-item">دانشگاه: دانشگاه شهید مدنی آذربایجان</li>
-                        </ul>
-                        <div class="card-footer p-1">
-                            <span class="card-info pr-4">قیمت:10000 تومان</span>
-                            <a href="#" class="btn btn-danger mr-auto">حذف آگهی</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card rtl">
-                        <img class="card-img-top  card-img-top-250" src="{{asset('assets/img/logo.png')}}" alt="Card image cap">
-                        <div class="card-body pb-1">
-                            <h5 class="card-title">کتاب شیمی</h5>
-                            <div id="summary">
-                                <p class="collapse" id="collapseSummary3">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
-                                </p>
-                                <a class="collapsed" data-toggle="collapse" href="#collapseSummary3" aria-expanded="false"
-                                   aria-controls="collapseSummary"></a>
-                            </div>
-                        </div>
-                        <ul class="list-group list-group-flush p-0 rtl">
-                            <li class="list-group-item">شهر: تهران</li>
-                            <li class="list-group-item">دانشگاه: دانشگاه شهید مدنی آذربایجان</li>
-                        </ul>
-                        <div class="card-footer p-1">
-                            <span class="card-info pr-4">قیمت:10000 تومان</span>
-                            <a href="#" class="btn btn-danger mr-auto">حذف آگهی</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card rtl">
-                        <img class="card-img-top  card-img-top-250" src="{{asset('assets/img/logo.png')}}" alt="Card image cap">
-                        <div class="card-body pb-1">
-                            <h5 class="card-title">کتاب شیمی</h5>
-                            <div id="summary">
-                                <p class="collapse" id="collapseSummary4">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
-                                </p>
-                                <a class="collapsed" data-toggle="collapse" href="#collapseSummary4" aria-expanded="false"
-                                   aria-controls="collapseSummary"></a>
-                            </div>
-                        </div>
-                        <ul class="list-group list-group-flush p-0 rtl">
-                            <li class="list-group-item">شهر: تهران</li>
-                            <li class="list-group-item">دانشگاه: دانشگاه شهید مدنی آذربایجان</li>
-                        </ul>
-                        <div class="card-footer p-1">
-                            <span class="card-info pr-4">قیمت:10000 تومان</span>
-                            <a href="#" class="btn btn-danger mr-auto">حذف آگهی</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card rtl">
                         <img class="card-img-top  card-img-top-250" src="{{asset('assets/img/logo.png')}}" alt="Card image cap">
                         <div class="card-body pb-1">
                             <h5 class="card-title">کتاب شیمی</h5>
                             <div id="summary">
                                 <p class="collapse" id="collapseSummary5">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                                    گرافیک است.
 
                                 </p>
-                                <a class="collapsed" data-toggle="collapse" href="#collapseSummary5" aria-expanded="false"
+                                <a class="collapsed" data-toggle="collapse" href="#collapseSummary5"
+                                   aria-expanded="false"
                                    aria-controls="collapseSummary"></a>
                             </div>
                         </div>
@@ -119,9 +35,9 @@
                             <li class="list-group-item">شهر: تهران</li>
                             <li class="list-group-item">دانشگاه: دانشگاه شهید مدنی آذربایجان</li>
                         </ul>
-                        <div class="card-footer p-1">
-                            <span class="card-info pr-4">قیمت:10000 تومان</span>
-                            <a href="#" class="btn btn-danger mr-auto">حذف آگهی</a>
+                        <div class="card-footer p-1 d-flex">
+                            <span class="card-info pl-2 pt-2 mr-auto">قیمت:10000 تومان</span>
+                            <a href="#" class="btn btn-danger mr-2">حذف</a>
                         </div>
                     </div>
                 </div>
